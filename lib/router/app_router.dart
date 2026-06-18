7983455a9edf8cb059fec6bc5d94a171f746822b
Yuliia Buhaiova <julia.bug0021@gmail.com>
2026-06-18 09:36:11 +0300

    feat: add UserProfileCubit provider

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_lab/lesson_11/homework_11_screen.dart';
 import 'package:flutter_lab/lesson_12/homework_12_screen.dart';
 import 'package:flutter_lab/lesson_18/homework_%D1%81ubit/homework_cubit_screen.dart';
@@ -5,6 +6,8 @@ import 'package:flutter_lab/lesson_18/homework_bloc/homework_bloc_screen.dart';
 import 'package:flutter_lab/lesson_18/state_managment_base_screen.dart';
 import 'package:flutter_lab/lesson_19/screens/rate_app_screen.dart';
 import 'package:flutter_lab/lesson_21/homework_animations.dart';
+import 'package:flutter_lab/lesson_22/error_handling_homework/data/repository/fake_user_repository.dart';
+import 'package:flutter_lab/lesson_22/error_handling_homework/presentation/cubit/user_profile_cubit.dart';
 import 'package:flutter_lab/lesson_22/error_handling_homework/presentation/ui/screens/user_profile_homework_screen.dart';
 import 'package:flutter_lab/router/page_names.dart';
 import 'package:flutter_lab/widgets_main_screen.dart';
@@ -70,7 +73,11 @@ final appRouter = GoRouter(
             GoRoute(
               path: 'error_handling',
               name: ScreenNames.lesson22,
-              builder: (context, state) => UserProfileHomeworkScreen(),
+              builder: (context, state) => BlocProvider(
+                create: (_) =>
+                    UserProfileCubit(FakeUserRepository())..loadUserProfile(),
+                child: UserProfileHomeworkScreen(),
+              ),
             ),
           ],
         ),
